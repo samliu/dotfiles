d4c4343d1d8c63171f11f58867f8de97acb3f94e
Sam Liu <sam@ambushnetworks.com>
2015-06-12 11:08:53 -0700

    upgraded oh-my-zsh

--- oh-my-zsh/lib/completion.zsh
+++ oh-my-zsh/lib/completion.zsh
@@ -1,4 +1,4 @@
-## fixme - the load process here seems a bit bizarre
+# fixme - the load process here seems a bit bizarre
 
 unsetopt menu_complete   # do not autoselect the first completion entry
 unsetopt flowcontrol
@@ -7,9 +7,6 @@ setopt complete_in_word
 setopt always_to_end
 
 WORDCHARS=''
-
-autoload -U compinit
-compinit -i
 
 zmodload -i zsh/complist
 
@@ -28,10 +25,40 @@ bindkey -M menuselect '^o' accept-and-infer-next-history
 
 zstyle ':completion:*:*:*:*:*' menu select
 zstyle ':completion:*:*:kill:*:processes' list-colors '=(#b) #([0-9]#) ([0-9a-z-]#)*=01;34=0=01'
-zstyle ':completion:*:*:*:*:processes' command "ps -u `whoami` -o pid,user,comm -w -w"
+if [ "$OSTYPE[0,7]" = "solaris" ]
+then
+  zstyle ':completion:*:*:*:*:processes' command "ps -u $USER -o pid,user,comm"
+else
+  zstyle ':completion:*:*:*:*:processes' command "ps -u $USER -o pid,user,comm -w -w"
+fi
 
-# Load known hosts file for auto-completion with ssh and scp commands
-if [ -f ~/.ssh/known_hosts ]; then
-  zstyle ':completion:*' hosts $( sed 's/[, ].*$//' $HOME/.ssh/known_hosts )
-  zstyle ':completion:*:*:(ssh|scp):*:*' hosts `sed 's/^\([^ ,]*\).*$/\1/' ~/.ssh/known_hosts`
+# disable named-directories autocompletion
+zstyle ':completion:*:cd:*' tag-order local-directories directory-stack path-directories
+
+# Use caching so that commands like apt and dpkg complete are useable
+zstyle ':completion::complete:*' use-cache 1
+zstyle ':completion::complete:*' cache-path $ZSH_CACHE_DIR
+
+# Don't complete uninteresting users
+zstyle ':completion:*:*:*:users' ignored-patterns \
+        adm amanda apache at avahi avahi-autoipd beaglidx bin cacti canna \
+        clamav daemon dbus distcache dnsmasq dovecot fax ftp games gdm \
+        gkrellmd gopher hacluster haldaemon halt hsqldb ident junkbust kdm \
+        ldap lp mail mailman mailnull man messagebus  mldonkey mysql nagios \
+        named netdump news nfsnobody nobody nscd ntp nut nx obsrun openvpn \
+        operator pcap polkitd postfix postgres privoxy pulse pvm quagga radvd \
+        rpc rpcuser rpm rtkit scard shutdown squid sshd statd svn sync tftp \
+        usbmux uucp vcsa wwwrun xfs '_*'
+
+# ... unless we really want to.
+zstyle '*' single-ignored show
+
+if [ "x$COMPLETION_WAITING_DOTS" = "xtrue" ]; then
+  expand-or-complete-with-dots() {
+    echo -n "\e[31m......\e[0m"
+    zle expand-or-complete
+    zle redisplay
+  }
+  zle -N expand-or-complete-with-dots
+  bindkey "^I" expand-or-complete-with-dots
 fi
--- oh-my-zsh/tools/install.sh
+++ oh-my-zsh/tools/install.sh
@@ -1,36 +1,52 @@
-if [ -d ~/.oh-my-zsh ]
-then
-  echo "You already have Oh My Zsh installed. You'll need to remove ~/.oh-my-zsh if you want to install"
+set -e
+
+if [ ! -n "$ZSH" ]; then
+  ZSH=~/.oh-my-zsh
+fi
+
+if [ -d "$ZSH" ]; then
+  echo "\033[0;33mYou already have Oh My Zsh installed.\033[0m You'll need to remove $ZSH if you want to install"
   exit
 fi
 
-echo "Cloning Oh My Zsh..."
-/usr/bin/env git clone git://github.com/robbyrussell/oh-my-zsh.git ~/.oh-my-zsh
+echo "\033[0;34mCloning Oh My Zsh...\033[0m"
+hash git >/dev/null 2>&1 && env git clone --depth=1 https://github.com/robbyrussell/oh-my-zsh.git $ZSH || {
+  echo "git not installed"
+  exit
+}
 
-echo "Looking for an existing zsh config..."
-if [ -f ~/.zshrc ] || [ -h ~/.zshrc ]
-then
-  echo "Found ~/.zshrc. Backing up to ~/.zshrc.pre-oh-my-zsh";
-  cp ~/.zshrc ~/.zshrc.pre-oh-my-zsh;
-  rm ~/.zshrc;
+echo "\033[0;34mLooking for an existing zsh config...\033[0m"
+if [ -f ~/.zshrc ] || [ -h ~/.zshrc ]; then
+  echo "\033[0;33mFound ~/.zshrc.\033[0m \033[0;32mBacking up to ~/.zshrc.pre-oh-my-zsh\033[0m";
+  mv ~/.zshrc ~/.zshrc.pre-oh-my-zsh;
 fi
 
-echo "Using the Oh My Zsh template file and adding it to ~/.zshrc"
-cp ~/.oh-my-zsh/templates/zshrc.zsh-template ~/.zshrc
+echo "\033[0;34mUsing the Oh My Zsh template file and adding it to ~/.zshrc\033[0m"
+cp $ZSH/templates/zshrc.zsh-template ~/.zshrc
+sed -i -e "/^export ZSH=/ c\\
+export ZSH=$ZSH
+" ~/.zshrc
 
-echo "Copying your current PATH and adding it to the end of ~/.zshrc for you."
-echo "export PATH=$PATH" >> ~/.zshrc
+echo "\033[0;34mCopying your current PATH and adding it to the end of ~/.zshrc for you.\033[0m"
+sed -i -e "/export PATH=/ c\\
+export PATH=\"$PATH\"
+" ~/.zshrc
 
-echo "Time to change your default shell to zsh!"
-chsh -s /bin/zsh
+TEST_CURRENT_SHELL=$(expr "$SHELL" : '.*/\(.*\)')
+if [ "$TEST_CURRENT_SHELL" != "zsh" ]; then
+    echo "\033[0;34mTime to change your default shell to zsh!\033[0m"
+    chsh -s $(grep /zsh$ /etc/shells | tail -1)
+fi
+unset TEST_CURRENT_SHELL
 
-echo '         __                                     __  '
-echo '  ____  / /_     ____ ___  __  __   ____  _____/ /_ '
-echo ' / __ \/ __ \   / __ `__ \/ / / /  /_  / / ___/ __ \ '
-echo '/ /_/ / / / /  / / / / / / /_/ /    / /_(__  ) / / / '
-echo '\____/_/ /_/  /_/ /_/ /_/\__, /    /___/____/_/ /_/  '
-echo '                        /____/'
-
-echo "\n\n ....is now installed."
-/bin/zsh
-source ~/.zshrc
+echo "\033[0;32m"'         __                                     __   '"\033[0m"
+echo "\033[0;32m"'  ____  / /_     ____ ___  __  __   ____  _____/ /_  '"\033[0m"
+echo "\033[0;32m"' / __ \/ __ \   / __ `__ \/ / / /  /_  / / ___/ __ \ '"\033[0m"
+echo "\033[0;32m"'/ /_/ / / / /  / / / / / / /_/ /    / /_(__  ) / / / '"\033[0m"
+echo "\033[0;32m"'\____/_/ /_/  /_/ /_/ /_/\__, /    /___/____/_/ /_/  '"\033[0m"
+echo "\033[0;32m"'                        /____/                       ....is now installed!'"\033[0m"
+echo "\n\n \033[0;32mPlease look over the ~/.zshrc file to select plugins, themes, and options.\033[0m"
+echo "\n\n \033[0;32mp.s. Follow us at http://twitter.com/ohmyzsh.\033[0m"
+echo "\n\n \033[0;32mp.p.s. Get stickers and t-shirts at http://shop.planetargon.com.\033[0m"
+env zsh
+. ~/.zshrc
